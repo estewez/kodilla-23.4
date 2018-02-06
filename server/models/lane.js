3cc853040aa7dbbb799617e931dbd97166d6f80c
Tomasz <estewez@gmail.com>
2018-02-06 14:48:52 +0100

    Task completed

--- server/models/lane.js
+++ server/models/lane.js
@@ -5,6 +5,7 @@ const laneSchema = new Schema({
   name: { type: 'String', required: true },
   notes: [{ type: Schema.ObjectId, ref: 'Note', required: true }],
   id: { type: 'String', required: true, unique: true },
+  editing: { type: 'Boolean', required: true, unique: true },
 }, {
   usePushEach: true,
 });
